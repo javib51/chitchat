14ecb63010cd38f538bfae5d32a99d448ff0dcf8
Javier Benitez <javier.benitezf51@gmail.com>
2018-11-14 12:46:46 +0200

    BE image resolution feature implemented

--- backend/image_thumbnail/functions/index.js
+++ backend/image_thumbnail/functions/index.js
@@ -12,9 +12,9 @@ const fs = require('fs');
 // Thumbnail prefix added to file names.
 const THUMB_PREFIX = 'thumb_';
 
-exports.generateThumbnail = functions.storage.object().onFinalize(async (object, context) => {
+exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
     // File and directory paths.
-    console.log(context);
+    console.log(object);
 
     const filePath = object.name;
     const fileDir = path.dirname(filePath);
@@ -22,8 +22,6 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object,
     const tmpFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
     const tempFile = path.join(os.tmpdir(), tmpFilePath);
     const tempDir = path.dirname(tempFile);
-
-    const maxResolution = 'full';
 
     // Exit if this is triggered on a file that is not an image.
     if (!object.contentType.startsWith('image/')) {
@@ -38,7 +36,7 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object,
     // Cloud Storage files.
     const bucket = admin.storage().bucket(object.bucket);
     const file = bucket.file(filePath);
-
+    const maxResolution = object.metadata.resolution;
 
 
     // Create the temp directory where the storage file will be downloaded.
@@ -47,17 +45,20 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object,
     await file.download({destination: tempFile});
     console.log('The file has been downloaded to', tempFile);
 
-    console.log('createThumb 640x480');
-    await createThumb(object, tempFile, bucket, file, 640, 480);
-
-
-    if(maxResolution == 'full' || maxResolution == '1280') {
-        console.log('createThumb 1280x960');
-        await createThumb(object, tempFile, bucket, file, 1280, 960);
+    /**
+     * Create thumb of lower resolutions only.
+     * Default image contains resolution defined
+     * on maxResolution variable.
+     * Ex: maxResolution = 'high' -> thumb low
+     */
+    if(maxResolution == 'high' || maxResolution == 'full') {
+        console.log('createThumb 640x480');
+        await createThumb(object, tempFile, bucket, file, 640, 480);
     }
 
-    if(maxResolution != 'full') {
-        object.bucket.
+    if(maxResolution == 'full') {
+        console.log('createThumb 1280x960');
+        await createThumb(object, tempFile, bucket, file, 1280, 960);
     }
 
     fs.unlinkSync(tempFile);
@@ -65,39 +66,43 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object,
 });
 
 async function createThumb(object, tempFile, bucket, file, max_width, max_height) {
-    const thumb_prefix = THUMB_PREFIX + max_width + "_" + max_height + "_";
-    const thumbFilePath = path.normalize(path.join(path.dirname(object.name), `${thumb_prefix}${path.basename(object.name)}`));
+    try {
+        const thumb_prefix = THUMB_PREFIX + max_width + "_" + max_height + "_";
+        const thumbFilePath = path.normalize(path.join(path.dirname(object.name), `${thumb_prefix}${path.basename(object.name)}`));
 
-    const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
+        const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
 
-    const thumbFile = bucket.file(thumbFilePath);
-    const metadata = {
-        contentType: object.contentType,
-    };
+        const thumbFile = bucket.file(thumbFilePath);
+        const metadata = {
+            contentType: object.contentType,
+        };
 
-    // Generate a thumbnail using ImageMagick.
-    await spawn('convert', [tempFile, '-thumbnail', `${max_width}x${max_height}>`, tempLocalThumbFile], {capture: ['stdout', 'stderr']});
-    console.log('Thumbnail created at', tempLocalThumbFile);
-    // Uploading the Thumbnail.
-    await bucket.upload(tempLocalThumbFile, {destination: thumbFilePath, metadata: metadata});
-    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
-    // Once the image has been uploaded delete the local files to free up disk space.
+        // Generate a thumbnail using ImageMagick.
+        await spawn('convert', [tempFile, '-thumbnail', `${max_width}x${max_height}>`, tempLocalThumbFile], {capture: ['stdout', 'stderr']});
+        console.log('Thumbnail created at', tempLocalThumbFile);
+        // Uploading the Thumbnail.
+        await bucket.upload(tempLocalThumbFile, {destination: thumbFilePath, metadata: metadata});
+        console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+        // Once the image has been uploaded delete the local files to free up disk space.
 
-    fs.unlinkSync(tempLocalThumbFile);
-    // Get the Signed URLs for the thumbnail and original image.
-    const config = {
-        action: 'read',
-        expires: '03-01-2500',
-    };
-    const results = await Promise.all([
-        thumbFile.getSignedUrl(config),
-        file.getSignedUrl(config),
-    ]);
-    console.log('Got Signed URLs.');
-    const thumbResult = results[0];
-    const originalResult = results[1];
-    const thumbFileUrl = thumbResult[0];
-    const fileUrl = originalResult[0];
-    // Add the URLs to the Database
-    await admin.database().ref('images').push({path: fileUrl, thumbnail: thumbFileUrl});
+        fs.unlinkSync(tempLocalThumbFile);
+        // Get the Signed URLs for the thumbnail and original image.
+        const config = {
+            action: 'read',
+            expires: '03-01-2500',
+        };
+        const results = await Promise.all([
+            thumbFile.getSignedUrl(config),
+            file.getSignedUrl(config),
+        ]);
+        console.log('Got Signed URLs.');
+        const thumbResult = results[0];
+        const originalResult = results[1];
+        const thumbFileUrl = thumbResult[0];
+        const fileUrl = originalResult[0];
+        // Add the URLs to the Database
+        await admin.database().ref('images').push({path: fileUrl, thumbnail: thumbFileUrl});
+    } catch(error){
+        console.error(error);
+    }
 }
