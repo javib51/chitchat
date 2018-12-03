81dc546149455bf55a5d5c64c5543d115a1e003c
Javier Benitez <javier.benitezf51@gmail.com>
2018-12-03 14:07:37 +0200

    join chat notification implemented

---
+++ backend/functions/chatNotifications.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+
+exports.createHandler = functions.firestore
+    .document('chats/{chatId}')
+    .onCreate((snap, context) => {
+        const chat = snap.data();
+        if (chat.type == "G") {
+            const title = chat.name;
+            const body = "Created group";
+            sendNotifications(chat.users, title, body);
+        }
+        return 0;
+    });
+
+exports.updateHandler = functions.firestore
+    .document('chats/{chatId}')
+    .onUpdate((change, context) => {
+
+        const newChat = change.after.data();
+        const oldChat = change.before.data();
+
+        if (newChat.type == "G") {
+            let users = newChat.users.slice();
+            users = users.filter(val => !oldChat.users.includes(val));
+
+            if(users.length > 0) {
+                console.log(users);
+                const title = newChat.name;
+                const body = "Added to group";
+                sendNotifications(users, title, body);
+            }
+        }
+        return 0;
+    });
+
+function sendNotifications(users, title, body) {
+    console.log("Send notifications");
+
+    const payload = {
+        notification: {
+            title: title,
+            body: body,
+            sound: 'default'
+        },
+        data: {
+            click_action: 'FLUTTER_NOTIFICATION_CLICK',
+            id: '1',
+            status: 'done',
+            title: title,
+            body: body,
+        },
+    };
+    users.forEach(userId => {
+        const userRef = admin.firestore().collection('users').doc(userId);
+        userRef.get().then(user => {
+            admin.messaging().sendToDevice(user.data().notificationToken, payload)
+                .then((response) => {
+                    // Response is a message ID string.
+                    console.log('Successfully sent message:', response);
+                })
+                .catch((error) => {
+                    console.log('Error sending message:', error);
+                });
+        });
+    });
+    console.log("Notifications sent");
+
+    return 0;
+}
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,6 +3,7 @@
 const admin = require('firebase-admin');
 const generateThumbnail = require('./generateThumbnail');
 const messageNotifications = require('./messageNotifications');
+const chatNotifications = require('./chatNotifications');
 
 //Gobal init
 admin.initializeApp();
@@ -13,3 +14,5 @@ admin.firestore().settings(settings);
 // Functions definition
 exports.generateThumbnail = generateThumbnail.handler;
 exports.messageNotifications = messageNotifications.handler;
+exports.chatNotificationsCreation = chatNotifications.createHandler;
+exports.chatNotificationsUpdate = chatNotifications.updateHandler;
