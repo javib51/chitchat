7d79d9cfa043432f27f1a027b3c1ffc7c65c8d34
Javier Benitez <javier.benitezf51@gmail.com>
2018-12-08 23:46:49 +0200

    new version of deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,12 @@
 #git checkout develop
 
 #Deploy firebase configuration and functions
-#cd backend/functions
-#npm install .
-#cd ..
-#firebase deploy --token "1/fW0DySUATxuYJs9lqHlNjwGpHhHf5kzl8oZE44SULGY"
+cd backend/functions
+npm install .
+cd ..
+firebase deploy --token "1/fW0DySUATxuYJs9lqHlNjwGpHhHf5kzl8oZE44SULGY"
 
-#cd ..
+cd ..
 
 #Download flutter
 if [ ! -d "flutter" ]; then
@@ -26,8 +26,12 @@ if [ ! -d "flutter" ]; then
 	curl -L https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v1.0.0-stable.tar.xz -o flutter.tar.xz
 	tar xf flutter.tar.xz
 fi
+
+#Prepare flutter to be executed
 export PATH=$PATH:`pwd`/flutter/bin
 cd frontend
+
+#Analyze environment
 flutter doctor -v
 
 #Deploy flutter application
